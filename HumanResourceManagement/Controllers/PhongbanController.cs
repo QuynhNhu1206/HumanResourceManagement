5c08d4d40592c65f233b76ac677d080bb00e0857
Phuc <ngocphucdn03@gmail.com>
2024-12-04 18:17:57 +0700

    nhan vien , phong ban

--- HumanResourceManagement/Controllers/PhongbanController.cs
+++ HumanResourceManagement/Controllers/PhongbanController.cs
@@ -3,15 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HumanResourceManagement.Data;
+using HumanResourceManagement.Models;
 
 namespace HumanResourceManagement.Controllers
 {
     public class PhongbanController : Controller
     {
-        // GET: Phongban
+
         public ActionResult Phongban()
         {
-            return View();
+            var db = new HumanResourceManagementEntities1();
+            var phongBans = db.PhongBans.ToList();
+
+            var viewModel = new PhongBanViewModel
+            {
+                DanhSachPhongBan = phongBans
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Phongban(PhongBanViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new HumanResourceManagementEntities1())
+                {
+                    db.PhongBans.Add(viewModel.PhongBanMoi);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Phongban");
+            }
+
+            return View(viewModel);
+        }
+        public ActionResult CapNhat(string id)
+        {
+            using (var db = new HumanResourceManagementEntities1())
+            {
+                // Tìm phòng ban cần cập nhật theo ID
+                var phongBan = db.PhongBans.FirstOrDefault(p => p.MaPhongBan == id);
+                if (phongBan == null)
+                {
+                    return HttpNotFound(); // Nếu không tìm thấy, trả về lỗi 404
+                }
+
+                // Tạo ViewModel và gửi sang View
+                var viewModel = new PhongBanViewModel
+                {
+                    PhongBanMoi = phongBan
+                };
+                return View(viewModel);
+            }
+        }
+
+        // Action POST: Cập nhật thông tin phòng ban
+        [HttpPost]
+        public ActionResult CapNhat(PhongBanViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new HumanResourceManagementEntities1())
+                {
+                    // Tìm phòng ban cần cập nhật
+                    var phongBan = db.PhongBans.FirstOrDefault(p => p.MaPhongBan == viewModel.PhongBanMoi.MaPhongBan);
+                    if (phongBan != null)
+                    {
+                        // Cập nhật thông tin
+                        phongBan.TenPhongBan = viewModel.PhongBanMoi.TenPhongBan;
+                        phongBan.SoLuongNhanVien = viewModel.PhongBanMoi.SoLuongNhanVien;
+                        phongBan.MoTa = viewModel.PhongBanMoi.MoTa;
+
+                        // Lưu thay đổi vào CSDL
+                        db.SaveChanges();
+                        return RedirectToAction("Phongban");
+                    }
+                }
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ HumanResourceManagement/Models/NhanVienViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HumanResourceManagement.Data;
+
+namespace HumanResourceManagement.Models
+{
+    public class NhanVienViewModel
+    {
+        public List<NhanVien> DanhSachNhanVien { get; set; }
+        public NhanVien NhanVienMoi { get; set; }
+        public NhanVien NhanVienUpdate { get; set; }
+        public SelectList PhongBanList { get; set; }
+        public SelectList ChucVuList { get; set; }
+        public SelectList TrinhDoList { get; set; }
+    }
+
+}
