6a980d30f91fdc218524e740453b8d306d0b97c0
Phuc <ngocphucdn03@gmail.com>
2024-12-10 10:46:24 +0700

    Login

--- HumanResourceManagement/Controllers/MyHomeController.cs
+++ HumanResourceManagement/Controllers/MyHomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using HumanResourceManagement.App_Data;
 
 namespace HumanResourceManagement.Controllers
 {
@@ -11,7 +13,14 @@ namespace HumanResourceManagement.Controllers
         // GET: MyHome
         public ActionResult MyHome()
         {
-            return View();
+            if (Session["user"] == null)
+            {
+               return RedirectToAction("Login");
+            } else
+            {
+                return View();
+
+            }
         }
         public ActionResult Login()
         {
@@ -20,12 +29,38 @@ namespace HumanResourceManagement.Controllers
         [HttpPost]
         public ActionResult Login(string user, string pass)
         {
-            if(user == "admin" && pass == "admin") {
+            HumanResourceManagementEntities db = new HumanResourceManagementEntities();
+            var TaiKhoan = db.TaiKhoans.SingleOrDefault(m=>m.TenTaiKhoan.ToLower() == user.ToLower()&& m.MatKhau == pass);
+            if(TaiKhoan != null)
+            {
+                Session["user"] = TaiKhoan;
+               
                 return RedirectToAction("MyHome");
             }
-            return View();
-        }
+            else
+            {
+                TempData["error"] = "Tên tài khoản hoặc mật khẩu không chính xác";
+                return View();
 
+            }
+            //if (user == "admin" && pass == "admin") {
+            //    Session["user"] = "admin";
+            //    return RedirectToAction("MyHome");
+            //}
+            //else
+            //{
+            //    TempData["error"] = "Tên tài khoản hoặc mật khẩu không chính xác";
+            //    return View();
+
+            //}
+        }
+        public ActionResult Logout()
+        {
+            Session.Remove("user");
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+
+        }
         public ActionResult UpdateInfo()
         {
             return View();
