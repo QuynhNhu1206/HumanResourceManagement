f8637a2eca7b9be230feddbfa2f28ff0b9e27bb0
quynhnhu1206 <leqnhu1206@gmail.com>
2024-12-11 02:29:20 +0700

    nhuchange1112

---
+++ HumanResourceManagement/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using HumanResourceManagement.Models;
+using HumanResourceManagement.App_Data;
+using System.Web.Mvc;
+using HumanResourceManagement.Profiles;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace HumanResourceManagement.App_Start
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+           
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+                cfg.CreateMap<HumanResourceManagement.Models.NhanVienModels, HumanResourceManagement.App_Data.NhanVien>();
+            });
+
+            
+            IMapper mapper = config.CreateMapper();
+            DependencyResolver.SetResolver(new DefaultDependencyResolver(mapper)); // Đăng ký vào MVC container
+        }
+    }
+
+    // Thêm lớp DefaultDependencyResolver để đăng ký IMapper vào container
+    public class DefaultDependencyResolver : IDependencyResolver
+    {
+        private readonly IMapper _mapper;
+
+        public DefaultDependencyResolver(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(IMapper))
+                return _mapper;
+
+            return null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return Enumerable.Empty<object>();
+        }
+    }
+}
---
+++ HumanResourceManagement/Controllers/BaseController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HumanResourceManagement.Controllers
+{
+    public class BaseController : Controller
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["HumanResourceManagementEntities"].ConnectionString;
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string userName = (string)filterContext.HttpContext.Session["User"];
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                // Nếu không có userName, có thể gán ViewBag để thông báo lỗi hoặc không thực hiện chuyển hướng ngay.
+                ViewBag.ErrorMessage = "Vui lòng đăng nhập!";
+
+                // Bạn có thể ngừng thực thi Action mà không cần chuyển hướng ngay:
+                // filterContext.Result = new ViewResult() { ViewName = "Login" };
+
+                // Tiếp tục hiển thị trang hiện tại thay vì chuyển hướng ngay lập tức
+                return;
+            }
+
+            string query = "SELECT HoTen FROM NhanVien WHERE TenTaiKhoan = @TenTaiKhoan";
+            string userFullName = string.Empty;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.Add("@TenTaiKhoan", System.Data.SqlDbType.NVarChar).Value = userName;
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    userFullName = reader["HoTen"].ToString();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                ViewBag.ErrorMessage = "Lỗi truy vấn: " + sqlEx.Message;
+                base.OnActionExecuting(filterContext);
+                return;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Lỗi hệ thống: " + ex.Message;
+                base.OnActionExecuting(filterContext);
+                return;
+            }
+            
+
+            ViewBag.UserName = string.IsNullOrEmpty(userFullName) ? "Tên không được cung cấp" : userFullName;
+            if (!string.IsNullOrEmpty(userFullName))
+            {
+                var nameParts = userFullName.Split(' ');
+                if (nameParts.Length > 1)
+                {
+                    
+                    ViewBag.ShortName = string.Join(" ", nameParts.Skip(1).Take(2));
+                }
+                else
+                {
+                    ViewBag.ShortName = userFullName;
+                }
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+    }
+}
