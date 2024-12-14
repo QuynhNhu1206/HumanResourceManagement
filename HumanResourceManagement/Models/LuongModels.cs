4f4c9beaceb30c4bfbd068271b3642a2c4823e18
02022003nha <02022003nha@gmail.com>
2024-12-14 14:43:25 +0700

    update hd 1412

---
+++ HumanResourceManagement/Models/LuongModels.cs
@@ -0,0 +1,25 @@
+﻿using HumanResourceManagement.App_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HumanResourceManagement.Models
+{
+    public class LuongModels
+    {
+            public string MaLuong { get; set; }
+            public decimal? LuongCoBan { get; set; } 
+            public decimal? HeSoLuongg { get; set; }
+            public decimal? PhuCapChucVu { get; set; }
+            public decimal? PhuCapTrinhDo { get; set; }
+            public int? ThamNienCongTac { get; set; }
+            public decimal? BHXH { get; set; }
+            public decimal? BHYT { get; set; }
+            public decimal? LuongThucNhan { get; set; }
+            public string MaNhanVien { get; set; }
+           public virtual NhanVien NhanVien { get; set; }
+        
+
+    }
+}
